6a623d9341c0f24824298e485fb015b0d2187bca
Alperen Akgün <alperenakgun12@hotmail.com>
2026-01-17 01:00:10 +0300

    fix for deploy frontendv1

--- src/pages/user/PerfumePages/PerfumeDetailsPage.tsx
+++ src/pages/user/PerfumePages/PerfumeDetailsPage.tsx
@@ -196,7 +196,7 @@ function PerfumeDetailsPage() {
               <Rating
                 name='parfume_rating'
                 value={rating}
-                onChange={(event, newValue: number) => setRating(newValue)}
+                onChange={(_, newValue: number) => setRating(newValue)}
                 precision={1}
               />
             </Grid>
