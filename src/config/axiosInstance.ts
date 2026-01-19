e432a8dbb7a6e5b0a94d203f9d1fec76a3eb0bde
Alperen Akgün <alperenakgun12@hotmail.com>
2026-01-19 23:12:24 +0300

    first user add

--- src/config/axiosInstance.ts
+++ src/config/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = import.meta.env.VITE_API_URL;
+export const BASE_URL = "https://perfumes-backend-blro.onrender.com";
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
